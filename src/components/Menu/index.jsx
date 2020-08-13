4c2bd45a3ad7f24b322e76631934fb0badd22d3b
Wesley de Souza <wesley.i476@gmail.com>
2020-08-13 15:51:32 -0300

    Criando menu sidenav

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -7,12 +7,25 @@ import cartIcon from '../../assets/img/Cart.png';
 import './styles.css';
 
 export default props => {
+
+    function openNav() {
+        document.querySelector('#sidenav').style.width = '27rem';
+        document.querySelector("nav").style.opacity = '0.9';
+    }
+
+    function closeNav() {
+        document.querySelector('#sidenav').style.width = '0';
+        document.querySelector("nav").style.opacity = '1';
+    }
+
     return (
         <header>
             <nav style={{backgroundColor: props.color}}>
                 <ul>
                     <li>
-                        <img src={menuIcon} alt="Icone de menu"/>
+                        <button onClick={openNav}>
+                            <img src={menuIcon} alt="Icone de menu"/>
+                        </button>
                     </li>
 
                     <li>
@@ -24,6 +37,15 @@ export default props => {
                     </li>
                 </ul>
             </nav>
+
+            <div className='sidenav' id='sidenav' style={{background: props.color}}>
+                <button onClick={closeNav} className='closebtn'>X</button>
+                <Link to='/'>Inicio</Link>
+                <Link to='#'>Carrinho</Link>
+                <Link to='/btsStore'>Map Of The Soul: 7</Link>
+                <Link to='/mxStore'>Yeah! We Rock the Show</Link>
+            </div>
+
         </header>
     )
 }
