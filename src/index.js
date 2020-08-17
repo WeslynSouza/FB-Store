8de3aaa92274668b1f8f9c0ffd374ed6cb514c82
Wesley de Souza <wesley.i476@gmail.com>
2020-08-17 11:56:10 -0300

    favicon

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 import App from './App';
+
+import icon from './assets/img/favicon.ico';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url={icon}/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
