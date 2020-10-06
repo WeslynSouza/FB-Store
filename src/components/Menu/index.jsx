295862563973e045babff64bf8853e2b7062782c
weslynSouza <wesley.i476@gmail.com>
2020-10-06 20:44:25 -0300

    Alterando menu e rodape

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { AiOutlineMenu } from 'react-icons/ai';
-import { FaSearch } from 'react-icons/fa';
-import cartIcon from '../../assets/img/Cart.png';
+import { FaSearch, FaBars, FaShoppingCart, FaUserCircle,
+     FaHome, FaMusic, FaChessRook, FaFilm, FaGamepad,FaChevronDown } from 'react-icons/fa'; 
 
 import './styles.css';
 
 export default props => {
 
     function openNav() {
-        document.querySelector('#sidenav').style.width = '27rem';
+        document.querySelector('#sidenav').style.width = '24rem';
         document.querySelector("nav").style.opacity = '0.9';
     }
 
@@ -27,7 +26,7 @@ export default props => {
       }else if(scrollTop <= 35){
         document.getElementById('nav').style.top="0";
       }else{
-        document.getElementById('nav').style.top="-6rem";
+        document.getElementById('nav').style.top="-10.4rem";
       }
       lastScrolltop = scrollTop;
     })
@@ -36,20 +35,21 @@ export default props => {
         <div>
             <header>
                 <nav className='navbar' id="nav">
-                    
-                    <div className="navbar-area">
-                        <button onClick={openNav} className='dropdown-button'>
-                            <AiOutlineMenu/>
-                        </button>
-
+                    <div className="logo-bar">
                         <Link to='/' className='logo'>fantastic baby store</Link>
+                    </div>
+
+                    <div className="navbar-area">
+                        <button onClick={openNav} className='menu-icon'>
+                            <FaBars/>
+                        </button>                        
 
                         <ul className='menu'>
-                            <li className="menu-item">Início</li>
-                            <li className="menu-item">K-Pop</li>
-                            <li className="menu-item">Animes</li>
-                            <li className="menu-item">Jogos</li>
-                            <li className="menu-item">Series</li>
+                            <li className="menu-item"><FaHome/> Início</li>
+                            <li className="menu-item"><FaMusic/> K-Pop</li>
+                            <li className="menu-item"><FaChessRook/> Animes</li>
+                            <li className="menu-item"><FaGamepad/> Jogos</li>
+                            <li className="menu-item"><FaFilm/> Series</li>
                         </ul>
 
                         <div className="search">
@@ -59,7 +59,20 @@ export default props => {
                             </button>
                         </div>
 
-                        <Link to='/Carrinho' className='icon-carrinho'><img src={cartIcon} alt="Icone de carrinho"/></Link>
+                        <div className="icons">
+                            <button className='search-icon'>
+                                <FaSearch/>
+                            </button>
+
+                            <Link to='/' className='user-icon'>
+                                <FaUserCircle/>
+                            </Link>
+
+                            <Link to='/Carrinho' className='carrinho-icon'>
+                                <FaShoppingCart/>
+                            </Link>
+                        </div>
+
                     </div>
 
                 </nav>
@@ -67,12 +80,27 @@ export default props => {
 
             <div className='sidenav' id='sidenav'>
                 <button onClick={closeNav} className='closebtn'>X</button>
-                <Link to='/'>Início</Link>
-                <Link to='#'>Carrinho</Link>
-                <Link to='/SobreNos'>Sobre nós</Link>
-                <Link to='/btsStore'>Map Of The Soul: 7</Link>
-                <Link to='/mxStore'>Yeah! We Rock the Show</Link>
-                <Link to='/bpStore'>How You Like That</Link>
+                <Link to='/'>
+                    <div><FaHome/> Início</div>
+                </Link>
+                <Link to='#'>
+                    <div><FaShoppingCart/> Carrinho</div>
+                </Link>
+                <Link to='#'>
+                    <div><FaMusic/> K-pop</div>
+                    <FaChevronDown/>
+                </Link>
+                <Link to='#'>
+                    <div><FaChessRook/> Animes</div>
+                </Link>
+                <Link to='#'>
+                    <div><FaFilm/> FIlmes/Series</div>
+                    <FaChevronDown/>
+                </Link>
+                <Link to='#'>
+                    <div><FaGamepad/> Jogos</div>
+                    <FaChevronDown/>
+                </Link>
             </div>
 
         </div>
--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
+
+import visa from '../../assets/img/visa.png';
+import master from '../../assets/img/master.png';
+import elo from '../../assets/img/elo.png';
+import hiper from '../../assets/img/hipercard.png';
+import ae from '../../assets/img/ae.png';
+import dc from '../../assets/img/dc.png';
+import boleto from '../../assets/img/boleto.png';
 
 import './styles.css';
 
@@ -7,11 +17,44 @@ export default props => {
         <footer>
             <h1>fantastic baby store</h1>
 
-            <p>A Fantastic Baby Store é uma loja de moda com foco em artigos de K-Pop, Geek e cultura pop no geral.</p>
+            <h2>Entre em contato</h2>
+            <p>Tel.:(00) 92222-1111</p>
+            <p>Email: Fbs@gmail.com</p>
 
-            <p>email@email.com<br/>(11)XXXX-XXXX</p>
+            <h2>Sobre a loja</h2>
+            <Link to='#'>Sobre nós</Link>
+            <Link to='#'>Pedidos custumizados</Link>
+            <Link to='#'>FAQ(Perguntas frequentes)</Link>
 
-            <h3>Soft Systems &copy; 2020</h3>
+            <h2>FBS nas redes sociais</h2>
+
+            <div className="redes">
+                <FaInstagram/>
+                <FaFacebook/>
+                <FaTwitter/>
+            </div>
+
+            <h2>Formas de pagamento (wirecard)</h2>
+
+            <div className="pagamento">
+                <div className="cartao">
+                    <h3>Cartões de credito</h3>
+                    <div className="pag-icons">
+                        <img src={visa} alt="visa"/>
+                        <img src={master} alt="mastercard"/>
+                        <img src={elo} alt="elo"/>
+                        <img src={hiper} alt="hipercard"/>
+                        <img src={ae} alt="amercian express"/>
+                        <img src={dc} alt="diner club"/>
+                    </div>
+                </div>
+                <div className="boleto">
+                    <h3>Boleto</h3>
+                    <img src={boleto} alt="boleto"/>
+                </div>
+            </div>
+
+            <h4>Soft Systems &copy; 2020</h4>
         </footer>
     )
 }
